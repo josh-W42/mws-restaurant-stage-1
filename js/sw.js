a0f0f9bd5feb64b4ffcc53534172cbaa042c3315
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-09-26 23:35:11 -0700

    last attempt at path error for sw

--- js/sw.js
+++ js/sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('restReview-v1').then(function(cache) {
       return cache.addAll([
-        'index.html',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'js/dbhelper.js',
-        'css/styles.css',
-        'data/restaurants.json',
+        '/index.html',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/css/styles.css',
+        '/data/restaurants.json',
       ]);
     })
   );
